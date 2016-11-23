1062b5449fbf8877e57ea1d1e003682c585452b0
Herman Schistad <hersch01@arm.com>
2016-11-23 17:05:04 +0000

    Add more extensive example suite for certificates

--- examples/development/certificate.py
+++ examples/development/certificate.py
@@ -1,4 +1,10 @@
+import sys
 from mbed_cloud_sdk.development.certificate import CertificateAPI
+
+DUMMY_PUB_KEY = """
+MFkwEwYHKoZIzj0CAQYIKoZIzj0DAQcDQgAENVRAgPj7Ra2K/BqejIhY+oSRylHS
+CdV7NZ9yEDGifNYYi0nZVpHwmbepoMbBW7jTJKjNDHaf8MhBsKPd0yyI4Q==
+"""
 
 def print_certificate(cert):
     body = "* %s (%s) *" % (cert.pub_key, cert.created_at)
@@ -9,10 +15,22 @@ def print_certificate(cert):
 def main():
     api = CertificateAPI()
     cert = api.get_certificate()
+
     if cert:
+        print "Current certificate:"
         print_certificate(cert)
-    else:
-        print "No certificate found"
+
+        api.revoke_certificate()
+        print "Revoked current certificate. Creating new one..."
+
+    # Get public key from first argument on command line
+    pub_key = DUMMY_PUB_KEY.strip().replace("\n","")
+    if len(sys.argv) == 2:
+        pub_key = sys.argv[1]
+
+    api.create_certificate(pub_key)
+    print "Created new certificate!"
+    print_certificate(api.get_certificate())
 
 if __name__ == "__main__":
     main()
