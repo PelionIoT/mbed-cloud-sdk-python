7da1d1d1fa691ddeaf66a320942a2a1e0a1bebe0
Herman Schistad <hersch01@arm.com>
2016-11-29 13:14:50 +0000

    Allow generate docs to have format as first argument

--- generate-docs.sh
+++ generate-docs.sh
@@ -2,10 +2,10 @@
 set -eo pipefail
 IFS=$'\n\t'
 
-OUTPUT_FORMAT="html";
+OUTPUT_FORMAT="${1:-html}";
 CONFIG_DIR="docs/";
 SOURCE_DIR="docs/";
-OUTPUT_DIR="docs/build/html";
+OUTPUT_DIR="docs/build/${OUTPUT_FORMAT}";
 
 # Build Sphinx docs
 sphinx-build -a -b $OUTPUT_FORMAT -c $CONFIG_DIR $SOURCE_DIR $OUTPUT_DIR;
