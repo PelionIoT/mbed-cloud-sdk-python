ce4965fca6f3de05702137397fc667517c9c2c5d
Herman Schistad <hersch01@arm.com>
2016-11-29 18:05:10 +0000

    Add functions for adding API keys + docstrings

--- mbed_cloud_sdk/access.py
+++ mbed_cloud_sdk/access.py
@@ -1,4 +1,5 @@
 """Functionality for access-related actions in mbed Cloud."""
+from __future__ import absolute_import
 import logging
 
 # Import common functions and exceptions from frontend API
@@ -33,24 +34,50 @@ class AccessAPI(BaseAPI):
             iam.configuration.host = config.get("host")
 
     @catch_exceptions(ApiException)
-    def list_api_keys(self):
-        """TODO: Write docstring."""
+    def list_api_keys(self, start=0, sort_by=None, sort_direction="asc"):
+        """List the API keys registered in the organisation.
+
+        :param start: Not yet implemented.
+        :param sort_by: Not yet implemented.
+        :param sort_direction: Not yet implemented.
+        """
         api = iam.DeveloperApi()
+
+        if start != 0 or sort_by is not None or sort_direction != "asc":
+            raise NotImplementedError("Sorting and pagination is not yet implemented")
 
         # Return the data array
         return api.get_all_api_keys().data
 
     @catch_exceptions(ApiException)
     def get_api_key(self, api_key):
-        """TODO: Write docstring."""
+        """Get API key details for key registered in organisation.
+
+        :param api_key: The key name (str)
+        """
         api = iam.DeveloperApi()
         return api.get_api_key(api_key)
 
     @catch_exceptions(ApiException)
     def delete_api_key(self, api_key):
-        """TODO: Write docstring."""
+        """Delete an API key registered in the organisation.
+
+        :param api_key: The key name (str)
+        """
         api = iam.DeveloperApi()
         return api.delete_api_key(api_key)
+
+    @catch_exceptions(ApiException)
+    def create_api_key(self, name, groups=[], owner=None):
+        """Create new API key registered to organisation.
+
+        :param name: The name of the API key (str)
+        :param groups: Optional list of group IDs (str)
+        :param owner: Optional user ID owning the API key (str)
+        """
+        api = iam.DeveloperApi()
+        body = iam.ApiKeyInfoReq(name=name, groups=groups, owner=owner)
+        return api.create_api_key(body)
 
     @catch_exceptions(ApiException)
     def list_groups(self):
