e4f01ebb3a15abda8f25d816afdaa4a346634da4
Herman Schistad <hersch01@arm.com>
2016-11-30 11:30:09 +0000

    Device log API is now read only

--- mbed_cloud_sdk/logging.py
+++ mbed_cloud_sdk/logging.py
@@ -7,7 +7,6 @@ from mbed_cloud_sdk import BaseAPI
 from mbed_cloud_sdk import config
 from mbed_cloud_sdk.decorators import catch_exceptions
 
-# TODO(Herman): Import backend API
 import mbed_cloud_sdk._backends.device_catalog as device_catalog
 from mbed_cloud_sdk._backends.device_catalog.rest import ApiException
 
@@ -51,35 +50,3 @@ class LoggingAPI(BaseAPI):
         """Get device log with provided ID."""
         api = device_catalog.DefaultApi()
         return api.device_log_retrieve(device_log_id)
-
-    @catch_exceptions(ApiException)
-    def delete_device_log(self, device_log_id, **kwargs):
-        """Delete device logs matching log ID and filters.
-
-        :param device_log_id: the device log ID to delete.
-        :param date_time: (Optional)
-        :param device_id: (Optional)
-        :param device_log_id2: (Optional)
-        :param event_type: (Optional)
-        :param state_change: (Optional)
-        :return: void
-        """
-        api = device_catalog.DefaultApi()
-        return api.device_log_destroy(device_log_id, **kwargs)
-
-    @catch_exceptions(ApiException)
-    def create_device_log(self, date_time, **kwargs):
-        """Delete device logs matching log ID and filters.
-
-        :param date_time: the date time (ISO-8601) string.
-        :param device_log_id: (Optional) (str)
-        :param event_type: (Optional) (str)
-        :param state_change: (Optional) (bool)
-        :param device_id: (Optional)
-        :param device_log_id2: (Optional)
-        :param event_type2: (Optional)
-        :param state_change2: (Optional)
-        :return: void
-        """
-        api = device_catalog.DefaultApi()
-        return api.device_log_create(date_time, **kwargs)
