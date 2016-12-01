e81a637e3f58dec9036102a3536d71d0134b4ed2
Herman Schistad <hersch01@arm.com>
2016-12-01 11:18:02 +0000

    Format the firmware list example

--- examples/update/list-firmware.py
+++ examples/update/list-firmware.py
@@ -6,8 +6,16 @@ def _main():
     api = UpdateAPI()
 
     images = api.list_firmware_images(limit=5, order='desc')
-    for i in images:
-        print(i)
+    date_fmt = "%Y-%m-%d %H:%M:%S"
+    for idx, image in enumerate(images):
+        description = image.description if image.description else "<No description>"
+        filename = image.datafile.rsplit("/", 1)[1]
+        print("%d) %s | %s [%s] | %s\n%s\n" % (idx,
+                                               image.created_at.strftime(date_fmt),
+                                               image.name,
+                                               image.image_id,
+                                               filename,
+                                               description))
 
 if __name__ == '__main__':
     _main()
