cdfb434c2c03bb7236cbc33fa05be92ac9a2315c
mbed Cloud (Circle CI) <support@mbed.com>
2016-12-12 17:18:15 +0000

    Add generated changes (2016-12-12 17:18:15.959811)

--- mbed_cloud_sdk/_backends/device_query_service/apis/default_api.py
+++ mbed_cloud_sdk/_backends/device_query_service/apis/default_api.py
@@ -157,7 +157,7 @@ class DefaultApi(object):
 
         # HTTP header `Content-Type`
         header_params['Content-Type'] = self.api_client.\
-            select_header_content_type([])
+            select_header_content_type(['application/x-www-form-urlencoded'])
 
         # Authentication setting
         auth_settings = ['Bearer']
