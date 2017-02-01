017c77fab5311c0ad5f2501ba2e14d224d4ce34c
Herman Schistad <hersch01@arm.com>
2017-02-01 12:28:42 +0000

    Add Copyright header and flake tests

--- examples/update/list-firmware.py
+++ examples/update/list-firmware.py
@@ -1,3 +1,16 @@
+# ---------------------------------------------------------------------------
+#   The confidential and proprietary information contained in this file may
+#   only be used by a person authorised under and to the extent permitted
+#   by a subsisting licensing agreement from ARM Limited or its affiliates.
+#
+#          (C) COPYRIGHT 2017 ARM Limited or its affiliates.
+#              ALL RIGHTS RESERVED
+#
+#   This entire notice must be reproduced on all copies of this file
+#   and copies of this file may only be made by a person if such person is
+#   permitted to do so under the terms of a subsisting license agreement
+#   from ARM Limited or its affiliates.
+# --------------------------------------------------------------------------
 """Example: listing firmware images and manifests using Update API."""
 from mbed_cloud.update import UpdateAPI
 import os
@@ -6,6 +19,7 @@ import string
 import sys
 
 DATE_FMT = "%Y-%m-%d %H:%M:%S"
+
 
 def _rand_id(N=6):
     return ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(N))
