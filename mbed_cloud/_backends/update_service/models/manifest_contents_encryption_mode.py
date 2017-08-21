2fa5ba11d3fe4572de5e2fb1fb232a82ca9600a1
mbed Cloud (Circle CI) <monty-bot@arm.com>
2017-08-21 14:48:32 +0000

    Add generated changes (2017-08-21 14:48:32.081408)

--- mbed_cloud/_backends/update_service/models/manifest_contents_encryption_mode.py
+++ mbed_cloud/_backends/update_service/models/manifest_contents_encryption_mode.py
@@ -44,7 +44,7 @@ class ManifestContentsEncryptionMode(object):
     def enum(self):
         """
         Gets the enum of this ManifestContentsEncryptionMode.
-        The encryption mode describing the kind of hashing, signing and, encryption in use. The following modes are available: 1: none-ecc-secp256r1-sha256: SHA-256 hashing, ECDSA signatures, using the secp256r1 curve. No payload encryption is used. 2: aes-128-ctr-ecc-secp256r1-sha256: SHA-256 hashing, ECDSA signatures, using the secp256r1 curve. The payload is encrypted with AES-128 in CTR-mode. 3: none-none-sha256: SHA-256 hashing. No signature is used. No payload encryption is used. This mode is not recommended except over existing, trusted connections.         
+        The encryption mode describing the kind of hashing, signing and, encryption in use. The following modes are available: 1: none-ecc-secp256r1-sha256: SHA-256 hashing, ECDSA signatures, using the secp256r1 curve. No payload encryption is used. 2: aes-128-ctr-ecc-secp256r1-sha256: SHA-256 hashing, ECDSA signatures, using the secp256r1 curve. The payload is encrypted with AES-128 in CTR-mode. 3: none-none-sha256: SHA-256 hashing. No signature is used. No payload encryption is used. This mode is not recommended except over existing, trusted connections. 
 
         :return: The enum of this ManifestContentsEncryptionMode.
         :rtype: int
@@ -55,7 +55,7 @@ class ManifestContentsEncryptionMode(object):
     def enum(self, enum):
         """
         Sets the enum of this ManifestContentsEncryptionMode.
-        The encryption mode describing the kind of hashing, signing and, encryption in use. The following modes are available: 1: none-ecc-secp256r1-sha256: SHA-256 hashing, ECDSA signatures, using the secp256r1 curve. No payload encryption is used. 2: aes-128-ctr-ecc-secp256r1-sha256: SHA-256 hashing, ECDSA signatures, using the secp256r1 curve. The payload is encrypted with AES-128 in CTR-mode. 3: none-none-sha256: SHA-256 hashing. No signature is used. No payload encryption is used. This mode is not recommended except over existing, trusted connections.         
+        The encryption mode describing the kind of hashing, signing and, encryption in use. The following modes are available: 1: none-ecc-secp256r1-sha256: SHA-256 hashing, ECDSA signatures, using the secp256r1 curve. No payload encryption is used. 2: aes-128-ctr-ecc-secp256r1-sha256: SHA-256 hashing, ECDSA signatures, using the secp256r1 curve. The payload is encrypted with AES-128 in CTR-mode. 3: none-none-sha256: SHA-256 hashing. No signature is used. No payload encryption is used. This mode is not recommended except over existing, trusted connections. 
 
         :param enum: The enum of this ManifestContentsEncryptionMode.
         :type: int
