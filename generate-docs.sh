7b875782a48f634695576a4ecc008b6cc9f01497
Rob Moran <github@thegecko.org>
2017-10-11 16:05:46 +0100

    Added build settings for test results (#147)
    * Added build settings for test results
    
    * Update circle.yml

--- generate-docs.sh
+++ generate-docs.sh
@@ -9,11 +9,3 @@ OUTPUT_DIR="docs/build/${OUTPUT_FORMAT}";
 
 # Build Sphinx docs
 sphinx-build -a -b $OUTPUT_FORMAT -c $CONFIG_DIR $SOURCE_DIR $OUTPUT_DIR;
-
-# If AWS_ID and SECRET is defined, we push to S3
-if [[ -n $AWS_ID && -n $AWS_SECRET ]]; then
-  export AWS_ACCESS_KEY_ID=$AWS_ID;
-  export AWS_SECRET_ACCESS_KEY=$AWS_SECRET;
-  export AWS_DEFAULT_REGION=us-west-2;
-  aws s3 sync --delete --cache-control max-age=3600 $OUTPUT_DIR s3://mbed-cloud-sdk-python;
-fi
