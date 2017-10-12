bca8d2132d275d836399fb5a7492dd4a800c2552
David Hyman <david.hyman@arm.com>
2017-10-12 10:19:30 +0100

    docker testrunner as a test

--- mbed_cloud/tests/test_with_rpc.py
+++ mbed_cloud/tests/test_with_rpc.py
@@ -1,14 +1,27 @@
 from mbed_cloud.tests.common import BaseCase
-import docker
+import os
+import sys
+import shlex
+import subprocess
 
 
 class TestWithRPC(BaseCase):
-    image = 'trunner'
+    image = "mbed/sdk-testrunner"
 
-    @staticmethod
+    @classmethod
     def setUpClass(cls):
-        cls.client = docker.from_env()
+        pass
+
+    def setUp(self):
+        exe = sys.executable
+        target = os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(os.path.dirname(__file__)))), 'tests', 'server.py')
+        self.process = subprocess.Popen(args=[exe, target])
 
     def test_run(self):
-        self.client.containers.run(image="trunner", network_mode='host')
+        cmd = shlex.split('docker run --rm --net="host" -p 5000:5000 -e "SERVER_URL=http://10.0.75.1:5000" mbed/sdk-testrunner')
+        subprocess.call(cmd)
 
+    def tearDown(self):
+        if self.process.poll():
+            raise subprocess.CalledProcessError('server exited prematurely')
+        self.process.kill()
