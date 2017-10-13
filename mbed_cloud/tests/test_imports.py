63d9829f5b816383387018841ae3d6f21c90762f
David Hyman <david.hyman@arm.com>
2017-10-13 13:36:09 +0100

    codecov

--- mbed_cloud/tests/test_imports.py
+++ mbed_cloud/tests/test_imports.py
@@ -4,4 +4,8 @@ from mbed_cloud.tests.common import BaseCase
 class TestImports(BaseCase):
     """A simple test for validating coverage etc"""
     def test_run(self):
-        from mbed_cloud import *  # noqa
+        from mbed_cloud import account_management
+        from mbed_cloud import certificates
+        from mbed_cloud import connect
+        from mbed_cloud import device_directory
+        from mbed_cloud import update
