532128f79373237ec2e98b03306d5feaea1566c0
David Hyman <david.hyman@arm.com>
2017-10-13 13:48:08 +0100

    fix metrics

--- version.py
+++ version.py
@@ -15,9 +15,11 @@
 # limitations under the License.
 # --------------------------------------------------------------------------
 
-# Version number is composed based on API backend version and SDK version.
-# Breaking changes in SDK will increment major version number.
-# API version number will follow Mbed release schedule (~quarterly releases).
+"""Version number is composed based on API backend version and SDK version.
+
+Breaking changes in SDK will increment major version number.
+API version number will follow Mbed release schedule (~quarterly releases).
+"""
 API_VERSION = "1.2"
 SDK_MAJOR_MINOR = "2"
 SDK_SUFFIX = ""
