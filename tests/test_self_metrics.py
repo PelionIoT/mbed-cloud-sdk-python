6135443e40b57d69011d520bd66e5e9e2cc719ca
David Hyman <david.hyman@arm.com>
2017-10-23 14:08:04 +0100

    missed a file

--- tests/test_self_metrics.py
+++ tests/test_self_metrics.py
@@ -1,4 +1,3 @@
-import os
 import subprocess
 
 import pytest
@@ -9,9 +8,8 @@ from tests.common import BaseCase
 @pytest.mark.meta
 class TestSelfMetrics(BaseCase):
     def test_self_metrics(self):
-        project_root = os.path.abspath(os.path.dirname(os.path.dirname((os.path.dirname(__file__)))))
-        subp = subprocess.Popen('flake8', cwd=project_root, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        subp = subprocess.Popen(['flake8', '.'], cwd=self._project_root_dir, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         subp.wait()
         if subp.returncode:
             indented = '\n'.join(['\t%s' % s for s in subp.stdout.readlines() + subp.stderr.readlines()])
-            raise ValueError('Saw flake8 failures running in %s:\n%s' % (project_root, indented))
+            raise ValueError('Saw flake8 failures running in %s:\n%s' % (self._project_root_dir, indented))
