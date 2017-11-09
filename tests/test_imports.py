58f2ddb258f7c9603a4eb61aa2465f04b111303c
David Hyman <david.hyman@arm.com>
2017-11-09 16:41:51 +0000

    config/import tests + unicode

--- tests/test_imports.py
+++ tests/test_imports.py
@@ -5,7 +5,7 @@ from tests.common import BaseCase
 import urllib3
 
 
-class TestImports(BaseCase):
+class Test(BaseCase):
     """A simple test for validating coverage"""
 
     def test_run(self):
@@ -15,33 +15,3 @@ class TestImports(BaseCase):
         from mbed_cloud import device_directory
         from mbed_cloud import update
         from mbed_cloud import _version
-
-
-class TestConfig(BaseCase):
-    def test_config_default(self):
-        # check host default from codegen is set to production
-        api = EndpointsApi()
-        self.assertIn('api.us-east-1', api.api_client.configuration.host)
-
-    def test_config_set_user_config(self):
-        # check top-level config setter
-        key = 'test_key'
-        config = {'api_key': key}
-        api = BaseAPI(config)
-        self.assertIn(key, api.config.get('api_key'))
-
-    def test_config_invalid_host(self):
-        # regression check - give a sane error for invalid hosts
-        api = connect.ConnectAPI(dict(host='https://0.0.0.0'))
-        with self.assertRaises(urllib3.exceptions.MaxRetryError):
-            api.list_connected_devices().data
-
-    def test_config_singleton(self):
-        # check two different api configs don't clobber each other
-        a = connect.ConnectAPI(dict(api_key='apple'))
-        b = connect.ConnectAPI(dict(api_key='banana'))
-        api_key = EndpointsApi
-        self.assertNotEqual(
-            a.apis[api_key].api_client.configuration.api_key,
-            b.apis[api_key].api_client.configuration.api_key
-        )
