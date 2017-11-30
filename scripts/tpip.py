0158b3193c618fb109d16837c2ab3251f727a58e
Graham Hammond <graham.hammond@me.com>
2017-11-30 09:52:46 +0000

    Change build step used to create TPIP report
    - The TPIP report should only include libraries used by the core SDK.
    - Improve script for flake8

--- scripts/tpip.py
+++ scripts/tpip.py
@@ -1,4 +1,21 @@
+# ---------------------------------------------------------------------------
+# Mbed Cloud Python SDK
+# (C) COPYRIGHT 2017 Arm Limited
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# --------------------------------------------------------------------------
 """Generate a Third Party Intellectual Property (TPIP) report.
+
 The report is output as a CSV file to the local directory.
 """
 
@@ -34,6 +51,7 @@ def get_metadata(item):
 
     return metadata_lines
 
+
 def process_metadata(pkg_name, metadata_lines):
     """Create a dictionary containing the relevent fields.
 
@@ -65,6 +83,7 @@ def process_metadata(pkg_name, metadata_lines):
     # Convert to a flat structure to be written out in the CSV
     return dict((key, '; '.join(value)) for (key, value) in tpip_pkg.items())
 
+
 def write_csv_file(tpip_pkgs):
     """Write the TPIP report out to a CSV file.
 
@@ -75,6 +94,7 @@ def write_csv_file(tpip_pkgs):
         writer.writeheader()
         for pkg_dict in tpip_pkgs:
             writer.writerow(pkg_dict)
+
 
 def main():
     """Generat a the TPIP report."""
