9a967dd1fa8d8146083f2e02aa2f068c1b249769
David Hyman <david.hyman@arm.com>
2018-01-09 10:04:24 +0000

    small amount of fallout from making paginator lazy

--- tests/test_config.py
+++ tests/test_config.py
@@ -37,7 +37,8 @@ class TestConfigObj(BaseCase):
         # regression check - give a sane error for invalid hosts
         api = ConnectAPI(dict(host='https://0.0.0.0'))
         with self.assertRaises(urllib3.exceptions.MaxRetryError):
-            api.list_connected_devices()
+            api.list_connected_devices().next()
+
 
     def test_config_singleton(self):
         # check two different api configs don't clobber each other
