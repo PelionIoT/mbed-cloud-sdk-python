5b68a45c7f9c320d7039d3ae6ff2bcae69e96dc4
David Hyman <david.hyman@arm.com>
2018-01-18 15:26:17 +0000

    change owner_id again

--- src/mbed_cloud/certificates/certificates.py
+++ src/mbed_cloud/certificates/certificates.py
@@ -221,7 +221,7 @@ class Certificate(BaseObject):
             "issuer": "issuer",
             "subject": "subject",
             "validity": "validity",
-            "owner_id": "owner",
+            "owner_id": "owner_id",
             "server_uri": "server_uri",
             "server_certificate": "server_certificate",
             "header_file": "security_file_content",
