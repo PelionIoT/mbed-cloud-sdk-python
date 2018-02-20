d29221472917a81723bc3a2c8ee646afd8dbed72
mbed Cloud (Circle CI) <monty-bot@arm.com>
2018-02-20 17:50:18 +0000

    device_directory via codegen @ 2018-02-20 17:50

--- src/mbed_cloud/_backends/device_directory/models/__init__.py
+++ src/mbed_cloud/_backends/device_directory/models/__init__.py
@@ -19,12 +19,9 @@ from .device_data_patch_request import DeviceDataPatchRequest
 from .device_data_post_request import DeviceDataPostRequest
 from .device_data_put_request import DeviceDataPutRequest
 from .device_event_data import DeviceEventData
-from .device_event_filter import DeviceEventFilter
 from .device_event_page import DeviceEventPage
-from .device_filter import DeviceFilter
 from .device_page import DevicePage
 from .device_query import DeviceQuery
-from .device_query_filter import DeviceQueryFilter
 from .device_query_page import DeviceQueryPage
 from .device_query_patch_request import DeviceQueryPatchRequest
 from .device_query_post_put_request import DeviceQueryPostPutRequest
