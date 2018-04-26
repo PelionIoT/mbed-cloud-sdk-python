2346ae4a393bf5a96d64f225a19b8f0bbf2a71f4
David Hyman <david.hyman@arm.com>
2018-04-26 11:19:21 +0100

    bdd device state test

--- tests/bdd/device_state.feature
+++ tests/bdd/device_state.feature
@@ -4,12 +4,13 @@ Feature: Subscribe to device events
     Scenario: Demonstrate timeout when getting first device event
         Given we have an MbedCloud
         When we subscribe to a registration device event
-            And we request the first value
-        Then we get a timeout
+            And we request the next value
+        Then we see a timeout
 
     @StopNotifications
     Scenario: Recieve a notification for registration event
         Given we have an MbedCloud
         When we subscribe to a registration device event
-            And we listen for notifications
+            And we request the next value
+            And a device changes state to REGISTERED
         Then we receive a notification
--- tests/bdd/steps/steps.py
+++ tests/bdd/steps/steps.py
@@ -1,33 +1,44 @@
 import behave
 
 import mbed_cloud
+import queue
 
 
 @behave.given(u'we have an MbedCloud')
 def step_impl(context):
-    context.api = mbed_cloud.ConnectAPI()
+    # for this sequence we don't really want a notification thread, so fake it:
+    context.api = mbed_cloud.ConnectAPI(dict(autostart_notification_thread=False))
+    context.api.ensure_notifications_thread = lambda: True
 
 
 @behave.when(u'we subscribe to a registration device event')
 def step_impl(context):
-    context.api.subscribe
+    context.observer = context.api.subscribe(mbed_cloud.channels.DeviceStateChanges())
 
 
-@behave.when(u'we request the first value')
+@behave.when(u'we request the next value')
 def step_impl(context):
-    raise NotImplementedError(u'STEP: When request the first value')
+    context.waitable = context.observer.next()
 
 
-@behave.then(u'we get a timeout')
+@behave.then(u'we see a timeout')
 def step_impl(context):
-    raise NotImplementedError(u'STEP: Then we get a timeout')
+    try:
+        context.waitable.block(timeout=0.1)
+    except (queue.Empty,):
+        # TODO: shouldn't the observer re-raise Empty as Timeout?!
+        pass
 
 
-@behave.when(u'we listen for notifications')
-def step_impl(context):
-    raise NotImplementedError(u'STEP: When listen for notifications')
+@behave.when(u'a device changes state to {state}')
+def step_impl(context, state):
+    # send a device state change notification
+    state_channel = {
+        'registered': mbed_cloud.channels._API_CHANNELS.registrations,
+    }[state.lower()]
+    context.api.subscribe.notify({state_channel: [{'device_id': '456'}]})
 
 
 @behave.then(u'we receive a notification')
 def step_impl(context):
-    raise NotImplementedError(u'STEP: Then we receive a notification')
+    context.waitable.block(timeout=1)
