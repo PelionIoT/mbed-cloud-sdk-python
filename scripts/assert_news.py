bd1404a3d1f7791a3bb476ee4e6e6cd50f41fb3d
David Hyman <david.hyman@arm.com>
2018-05-03 14:07:54 +0100

    more thorough

--- scripts/assert_news.py
+++ scripts/assert_news.py
@@ -10,24 +10,48 @@ def main(news_dir=None):
     if news_dir is None:
         from generate_news import news_dir
     news_dir = os.path.abspath(news_dir)
-    added_news = subprocess.check_output(
-        ['git', 'diff', 'HEAD', '--name-status', news_dir],
+
+    # figure out what the 'default branch' for the origin is
+    origin_stats = subprocess.check_output(
+        ['git', 'remote', 'show', 'origin'],
         cwd=news_dir
     ).decode()
-    if not added_news:
-        print('🚫️ Error: Did not find any news files!\nPlease add a news file to `%s`' % (news_dir,))
-        exit(1)  # exit with an error status, no need for a traceback
-    print('✔️ %s new files in `%s`' % (len(added_news.splitlines()), news_dir))
+
+    # the output of the git command looks like:
+    # '  HEAD branch: master'
+    origin_branch = 'master'  # unless we prove otherwise
+    for line in origin_stats.splitlines():
+        if 'head branch:' in line.lower():
+            origin_branch = line.split(':', 1)[-1].strip()
+            break
+
+    # figure out the current branch
+    current_branch = subprocess.check_output(
+        ['git', 'rev-parse', '--abbrev-ref', 'HEAD'],
+        cwd=news_dir
+    ).decode().strip()
+
+    print('🔎Finding news in `%s` to add to remote `%s`' % (current_branch, origin_branch))
+    file_diff = subprocess.check_output(
+        ['git', 'diff', '%s...%s' % (origin_branch, current_branch), '--name-status', news_dir],
+        cwd=news_dir
+    ).decode()
 
     # the output of the git command looks like:
     # 'A       docs/news/789.feature'
 
     # [optional] ensure we have an addition, rather than just modify/delete
-    # for line in added_news.splitlines():
-    #     if line.startswith('A'):
-    #         break
-    # else:
-    #     raise Exception('No news files additions!\nAdd a news file to `%s`' % (news_dir,))
+    added_news = [line for line in file_diff.splitlines() if line.lower().strip().startswith('a')]
+
+    # pass or fail
+    if not added_news:
+        print(
+            '🚫️ Error: Uh-oh, did not find any news files!\n'
+            '❗ Please add a news file to `%s`\n'
+            '± File diff:\n%s' % (news_dir, file_diff)
+        )
+        exit(1)  # exit with an error status, no need for a traceback
+    print('✔️ %s new files in `%s`' % (len(added_news), news_dir))
 
 
 if __name__ == '__main__':
