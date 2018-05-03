59772b415ab65f940637aea24f280be56358e4b8
David Hyman <david.hyman@arm.com>
2018-05-03 11:08:21 +0100

    nice news warnings

--- scripts/assert_news.py
+++ scripts/assert_news.py
@@ -1,15 +1,35 @@
-from generate_news import news_dir
+# -*- coding: utf8 -*-
 
-import os
 import subprocess
+import sys
+import os
 
 
-def main():
+def main(news_dir=None):
     """Checks for existence of a new newsfile"""
-    subprocess.check_call(
-        ['git', 'diff', ''],
+    if news_dir is None:
+        from generate_news import news_dir
+    news_dir = os.path.abspath(news_dir)
+    added_news = subprocess.check_output(
+        ['git', 'diff', 'HEAD', '--name-status', news_dir],
         cwd=news_dir
-    )
+    ).decode()
+    if not added_news:
+        print('🚫️ Error: No new file changes found!\nPlease add a news file to `%s`' % (news_dir,))
+        exit(1)  # exit with an error status, no need for a traceback
+    print('✔️ %s new files in `%s`' % (len(added_news.splitlines()), news_dir))
+
+    # the output of the git command looks like:
+    # 'A       docs/news/789.feature'
+
+    # [optional] ensure we have an addition, rather than just modify/delete
+    # for line in added_news.splitlines():
+    #     if line.startswith('A'):
+    #         break
+    # else:
+    #     raise Exception('No news files additions!\nAdd a news file to `%s`' % (news_dir,))
+
 
 if __name__ == '__main__':
-    main()
+    news_dir = sys.argv[1] if len(sys.argv) > 1 else None
+    main(news_dir)
