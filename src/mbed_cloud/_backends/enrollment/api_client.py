94143c3c8b2777d56a5f753ba04fd03ce7896a00
Monty <monty-bot@arm.com>
2018-05-17 11:28:57 +0100

    Codegen changes [swagger-codegen/enrollment] (2018-04-26 14:06) (#49)
    * enrollment via codegen @ 2018-04-26 14:05
    
    * enrollment via codegen @ 2018-05-11 10:02

--- src/mbed_cloud/_backends/enrollment/api_client.py
+++ src/mbed_cloud/_backends/enrollment/api_client.py
@@ -2,7 +2,7 @@
 """
     Enrollment API
 
-    Mbed Cloud Connect Enrollment Service allows users to claim the ownership of a device which is not yet assigned to an account. A device without an assigned account can be a device purchased from the open market (OEM dealer) or a device trasferred from an account to another. More information in [Device overship: First-to-claim](TODO: link needed) document. 
+    Mbed Cloud Connect Enrollment Service allows users to claim the ownership of a device which is not yet assigned to an account. A device without an assigned account can be a device purchased from the open market (OEM dealer) or a device transferred from an account to another. More information in [Device ownership: First-to-claim](https://cloud.mbed.com/docs/current/connecting/device-ownership.html) document. 
 
     OpenAPI spec version: 3
     
