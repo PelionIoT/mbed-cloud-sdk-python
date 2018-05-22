3468ec026245f5bba76d6babe13c0222d88c20ba
David Hyman <david.hyman@arm.com>
2018-05-22 10:08:29 +0100

    🌈 bootstrap cleanup

--- src/mbed_cloud/bootstrap/bootstrap.py
+++ src/mbed_cloud/bootstrap/bootstrap.py
@@ -62,8 +62,10 @@ class BootstrapAPI(BaseAPI):
 
 class PreSharedKey(BaseObject):
     """Describes device object from the catalog.
-    For more information about such keys, 
-    have a look at https://cloud.mbed.com/docs/latest/connecting/mbed-client-lite-security-considerations.html"
+
+    For more information about such keys,
+    have a look at
+    https://cloud.mbed.com/docs/latest/connecting/mbed-client-lite-security-considerations.html"
     """
 
     @staticmethod
@@ -79,7 +81,7 @@ class PreSharedKey(BaseObject):
 
         Endpoint name is the unique ID of the pre-shared key.
         16-64 printable (non-control) ASCII characters.
-        It also must be globally unique. 
+        It also must be globally unique.
         Consider using vendor-MAC-ID-device-model.
         For example "myEndpoint.host.com"
 
@@ -96,7 +98,7 @@ class PreSharedKey(BaseObject):
         - It is not case sensitive; 4a is same as 4A
         - It is allowed with or without 0x in the beginning.
         - The minimum length of the secret is 128 bits and max 512 bits.
-        
+
         For example "4a4a4a4a4a4a4a4a4a4a4a4a4a4a4a4a"
 
         :return: The secret_hex of this PreSharedKey.
