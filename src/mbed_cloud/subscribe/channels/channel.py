ad6eb85228b28ccbce343b83965e9260c5dc2144
David Hyman <david.hyman@arm.com>
2018-06-18 10:02:19 +0100

    channels can be named

--- src/mbed_cloud/subscribe/channels/channel.py
+++ src/mbed_cloud/subscribe/channels/channel.py
@@ -79,6 +79,7 @@ class ChannelSubscription(object):
         self._optional_filters = None
         self._route_keys = None
         self.add_filter_function(self._filter_optional_keys)
+        self.name = None
         super(ChannelSubscription, self).__init__()
 
     def get_routing_keys(self):
@@ -139,6 +140,10 @@ class ChannelSubscription(object):
     def __exit__(self, exc_type, exc_value, traceback):
         """Exit"""
         return self.ensure_stopped()
+
+    def __repr__(self):
+        """String representation"""
+        return '<%s %s>' % (self.__class__.__name__, self.name or self._route_keys)
 
     def start(self):
         """Base method for starting the channel"""
