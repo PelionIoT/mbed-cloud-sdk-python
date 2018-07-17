1aa546479111cd8ea26e67d05627d412cf21b9ff
David Hyman <david.hyman@arm.com>
2018-07-17 11:13:19 +0100

    packaging

---
+++ scripts/auto_version/auto_version/__main__.py
@@ -0,0 +1,3 @@
+from auto_version_tools import main
+
+__name__ == '__main__' and main()
---
+++ scripts/auto_version/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+   name='auto_version',
+   long_description='Tool for managing SemVer versioning of a project.',
+   license='Apache 2.0',
+   version='2018',
+   packages=['auto_version'],
+   install_requires=['toml']
+)
