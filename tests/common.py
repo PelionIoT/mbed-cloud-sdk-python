68e762d8c319cb1aaac044e1268ed56df5a2afdf
David Hyman <david.hyman@arm.com>
2018-09-03 09:46:36 +0100

    staticmethod

--- tests/common.py
+++ tests/common.py
@@ -5,4 +5,4 @@ import unittest
 class BaseCase(unittest.TestCase):
     # path assuming that this file is at `tests\common.py`
     _project_root_dir = os.path.abspath(os.path.dirname(os.path.dirname(__file__)))
-    _skip_in_ci = unittest.skipIf(os.environ.get('CI'), 'Do not run in CI')
+    _skip_in_ci = staticmethod(unittest.skipIf(os.environ.get('CI'), 'Do not run in CI'))
