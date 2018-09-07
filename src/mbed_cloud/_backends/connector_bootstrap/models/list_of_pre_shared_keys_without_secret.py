a848ad92ca8439bb872f10377d7bc8f681f4dd10
mbed Cloud (Circle CI) <monty-bot@arm.com>
2018-09-07 10:17:46 +0000

    connector_bootstrap via codegen @ 2018-09-07 10:17

--- src/mbed_cloud/_backends/connector_bootstrap/models/list_of_pre_shared_keys_without_secret.py
+++ src/mbed_cloud/_backends/connector_bootstrap/models/list_of_pre_shared_keys_without_secret.py
@@ -3,7 +3,7 @@
 """
     Bootstrap API
 
-    Mbed Cloud Bootstrap API allows web applications to control the device bootstrapping process.
+    Bootstrap API allows web applications to control the device bootstrapping process.
 
     OpenAPI spec version: 2
     
