0247a729bd227912fe9c651c52b08981af5da68b
David Hyman <david.hyman@arm.com>
2018-09-10 18:27:15 +0100

    🥒first pass at new interpreter for foundation bdd, using behave

--- tests/bdd/steps/steps.py
+++ tests/bdd/steps/steps.py
@@ -1,44 +1,77 @@
 import behave
 
+import functools
+
 import mbed_cloud
-import queue
+from mbed_cloud.sdk import entities
+from mbed_cloud import sdk
+
+import ast
 
 
-@behave.given(u'we have an MbedCloud')
-def step_impl(context):
-    # for this sequence we don't really want a notification thread, so fake it:
-    context.api = mbed_cloud.ConnectAPI(dict(autostart_notification_thread=False))
-    context.api.ensure_notifications_thread = lambda: True
+#
+# def when_or_given(*behave_args):
+#     def decorator():
+#         def wrapped():
+#         behave.given()
 
 
-@behave.when(u'we subscribe to a registration device event')
-def step_impl(context):
-    context.observer = context.api.subscribe(mbed_cloud.channels.DeviceStateChanges())
+def is_not_equal(a, b):
+    if a == b:
+        raise AssertionError('%s == %s' % (a, b))
 
 
-@behave.when(u'we request the next value')
-def step_impl(context):
-    context.waitable = context.observer.next()
+def is_equal(a, b):
+    if a != b:
+        raise AssertionError('%s != %s' % (a, b))
 
 
-@behave.then(u'we see a timeout')
-def step_impl(context):
-    try:
-        context.waitable.block(timeout=0.1)
-    except (queue.Empty,):
-        # TODO: shouldn't the observer re-raise Empty as Timeout?!
-        pass
+@behave.given(u'we have an {entity}')
+@behave.when(u'we have an {entity}')
+@behave.given(u'we have a {entity}')
+@behave.when(u'we have a {entity}')
+@behave.given(u'we have an {entity} called {name}')
+@behave.when(u'we have an {entity} called {name}')
+@behave.given(u'we have a {entity} called {name}')
+@behave.when(u'we have a {entity} called {name}')
+def step_impl(context, entity, name=None):
+    context.entities = getattr(context, 'entities', {})
+    key = name or entity.__class__.__name__
+    klass = getattr(entities, entity, None) or getattr(sdk, entity)
+    context.entities[key] = klass()
 
 
-@behave.when(u'a device changes state to {state}')
-def step_impl(context, state):
-    # send a device state change notification
-    state_channel = {
-        'registered': mbed_cloud.channels._API_CHANNELS.registrations,
-    }[state.lower()]
-    context.api.subscribe.notify({state_channel: [{'device_id': '456'}]})
+@behave.when(u'we set {name} {attr} to literally {value}')
+@behave.given(u'we set {name} {attr} to literally {value}')
+def step_impl(context, name, attr, value):
+    setattr(context.entities[name], attr, ast.literal_eval(value))
 
 
-@behave.then(u'we receive a notification')
-def step_impl(context):
-    context.waitable.block(timeout=1)
+@behave.when(u'we set {name} {attr} to {other_name} {other_attr}')
+def step_impl(context, name, attr, other_name, other_attr):
+    set_to = getattr(context.entities[other_name], other_attr)
+    setattr(context.entities[name], attr, set_to)
+
+
+@behave.then(u'{name} {attr} is {operator} {other_name} {attr}')
+def step_impl(context, name, attr, operator, other_name, other_attr):
+    first = getattr(context.entities[name], attr)
+    second = getattr(context.entities[other_name], other_attr)
+    if operator == 'equivalent to':
+        is_equal(first, second)
+    elif operator == 'not equivalent to':
+        is_not_equal(first, second)
+
+
+@behave.then(u'{name} object is {operator} {other_name}')
+def step_impl(context, name, operator, other_name):
+    if operator == 'equivalent to':
+        is_equal(context.entities[name], context.entities[other_name])
+    elif operator == 'not equivalent to':
+        is_not_equal(context.entities[name], context.entities[other_name])
+
+
+@behave.then(u'{name} {attr} is literally {value}')
+def step_impl(context, name, attr, value):
+    first = getattr(context.entities[name], attr)
+    is_equal(first, ast.literal_eval(value))
