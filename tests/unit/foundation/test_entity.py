3ce50f697aab0b63781d1251a7990141f54db7c7
Graham Hammond <graham.hammond@arm.com>
2019-02-06 16:54:49 +0000

    Python 2 compatibility for tests

--- tests/unit/foundation/test_entity.py
+++ tests/unit/foundation/test_entity.py
@@ -33,7 +33,7 @@ class SimpleEntity(Entity):
             a_datetime_field=None,
             a_date_field=None,
     ):
-        super().__init__(_client=_client)
+        super(SimpleEntity).__init__(_client=_client)
 
         # inline imports for avoiding circular references and bulk imports
 
