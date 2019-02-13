b96117a559665a61ed55f3f435f385f27c9b529f
Graham Hammond <graham.hammond@arm.com>
2019-02-13 18:31:53 +0000

    Try 3

--- scripts/generate_ci_config.py
+++ scripts/generate_ci_config.py
@@ -172,7 +172,7 @@ def new_foundation_gen():
     template = yaml.safe_load("""
     steps:
       - checkout
-      - run: pip install pipenv
+      - run: sudo pip install pipenv
       - run: pipenv install --dev . 
       - run:
           pipenv run python scripts/foundation/render_sdk.py 
