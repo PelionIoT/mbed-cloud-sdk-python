fbe81d1a10e00c2e90acfc270baf063c379f9651
monty <monty-bot@arm.com>
2019-02-14 11:38:40 +0000

    Auto-generated code

--- src/mbed_cloud/sdk/_modules/accounts/__init__.py
+++ src/mbed_cloud/sdk/_modules/accounts/__init__.py
@@ -1,5 +1,5 @@
 """
 Provides structure to expose UserInvitation functionality grouped as a module.
 
-This file is auto-generated from API Specifications
+This file is auto-generated from API Specifications.
 """
--- src/mbed_cloud/sdk/_modules/security/__init__.py
+++ src/mbed_cloud/sdk/_modules/security/__init__.py
@@ -1,5 +1,5 @@
 """
 Provides structure to expose VerificationResponse functionality grouped as a module.
 
-This file is auto-generated from API Specifications
+This file is auto-generated from API Specifications.
 """
