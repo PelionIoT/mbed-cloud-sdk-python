85328862631baf1d847b313fb1a42f894862e444
Graham Hammond <graham.hammond@arm.com>
2019-03-07 18:57:57 +0000

    Whitespace change

--- src/mbed_cloud/foundation/_custom_methods/common.py
+++ src/mbed_cloud/foundation/_custom_methods/common.py
@@ -5,6 +5,7 @@ from mbed_cloud.pagination import PaginatedResponse
 
 def paginate(self, foreign_key, wraps, *args, **kwargs):
     """Pagination wrapper for listing resources."""
+
     def wrapper(api_data):
         return foreign_key().from_api(**api_data)
 
