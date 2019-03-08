5d3ea0b5d2190bd6cd0bf26cc99b8e4cf4981ec0
Graham Hammond <graham.hammond@arm.com>
2019-03-08 09:56:05 +0000

    Flake8 fix

--- src/mbed_cloud/foundation/_custom_methods/common.py
+++ src/mbed_cloud/foundation/_custom_methods/common.py
@@ -5,7 +5,6 @@ from mbed_cloud.pagination import PaginatedResponse
 
 def paginate(self, foreign_key, wraps, *args, **kwargs):
     """Pagination wrapper for listing resources."""
-
     def wrapper(api_data):
         return foreign_key().from_api(**api_data)
 
